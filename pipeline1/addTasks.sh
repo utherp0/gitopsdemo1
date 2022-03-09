f8b363a255bd386f9b1074a07da650e7c194e621
utherp0 <ian.lawson@redhat.com>
2022-03-09 14:06:46 +0000

    Added manual task add for tasks for maven pipeline, plus maven build (working) pipeline

---
+++ pipeline1/addTasks.sh
@@ -0,0 +1,2 @@
+oc apply -f https://raw.githubusercontent.com/tektoncd/catalog/main/task/git-clone/0.5/git-clone.yaml
+oc apply -f https://raw.githubusercontent.com/tektoncd/catalog/main/task/maven/0.2/maven.yaml
